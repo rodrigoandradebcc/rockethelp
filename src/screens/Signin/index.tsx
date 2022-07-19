66d4ee337d065d2805106b9a04072d497207ab68
rcandrade <rcandrade@rdm504017.local>
2022-07-19 10:11:20 -0300

    feature: class one

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,41 @@
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import Logo from '../../assets/logo_primary.svg';
+import { Input } from "../Input";
+import { Button } from "../Button";
+
+import { Envelope, Key } from 'phosphor-react-native';
+import { useState } from "react";
+
+export function Signin(){
+    const {colors} = useTheme();
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignIn(){
+        console.log(name, password);
+    }
+
+
+    return(
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
+            <Logo />
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>Acesse sua conta</Heading>
+
+            <Input
+                placeholder="E-mail"
+                mb={4} InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]} />}/>} 
+                onChangeText={setName}
+            />
+            <Input
+                mb={8}
+                placeholder="Password"
+                InputLeftElement={<Icon ml={4} as={<Key color={colors.gray[300]} />}/>}
+                secureTextEntry
+                onChangeText={setPassword}
+            />
+
+            
+            <Button title="Entrar" w="full" onPress={handleSignIn}/>
+        </VStack>
+    )
+}
